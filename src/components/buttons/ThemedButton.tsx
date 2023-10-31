3aea2195da5221574480d39617c661ad15d38bb4
Ori <oricho123@gmail.conm>
2023-10-31 13:17:41 +0200

    Change ThemedButton.tsx styles a bit

--- src/components/buttons/ThemedButton.tsx
+++ src/components/buttons/ThemedButton.tsx
@@ -3,5 +3,5 @@ import {ComponentProps} from 'react';
 import styles from "./styles.module.css"
 
 export const ThemedButton = ({children, ...props}: ComponentProps<"button">) => {
-    return <button className={styles.themedButton} {...props}>{children}</button>;
+    return <button className={styles['themed-button']} {...props}>{children}</button>;
 };
