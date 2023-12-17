87fc7b54cd61a754da0f3eed429c9c89e9a4a9f1
Ori Lael <oricho123@gmail.com>
2023-12-17 20:53:15 +0200

    Add logout option

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -8,22 +8,25 @@ type providerProps = {
 type AuthContextType = {
     token: string | null;
     setToken: (value: string, expires_ms?: number) => void;
+    deleteToken: () => void;
 };
 
 const initialValue = {
     token: null,
     setToken: () => {
+    },
+    deleteToken: () => {
     }
 };
 
 const AuthContext = createContext<AuthContextType>(initialValue);
 
 const AuthProvider = ({children}: providerProps) => {
-    const [token, setToken] = useCookie('token');
+    const [token, setToken, deleteToken] = useCookie('token');
 
 
     return (
-        <AuthContext.Provider value={{token, setToken}}>
+        <AuthContext.Provider value={{token, setToken, deleteToken}}>
             {children}
         </AuthContext.Provider>
     )
