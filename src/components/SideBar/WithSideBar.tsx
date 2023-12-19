be553dbb0608248e483a0bb6fdf12c8b47c51ca9
Ori <oricho123@gmail.conm>
2023-12-19 20:37:04 +0200

    Add SideBar skeleton

---
+++ src/components/SideBar/WithSideBar.tsx
@@ -0,0 +1,14 @@
+// @flow
+import React from 'react';
+import styles from "./styles.module.css"
+import {SideBar} from "./SideBar";
+
+
+export function WithSideBar({children, style}: React.ComponentProps<'div'>) {
+    return (
+        <div style={style} className={styles['page-wrapper']}>
+            <SideBar/>
+            {children}
+        </div>
+    );
+}
